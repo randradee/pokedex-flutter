2a120969600e0425cbd817840c9c6607c0bcada6
randradee <dev.renatopandrade@gmail.com>
2024-05-01 20:52:17 -0300

    feat: tela de listagem com layout básico

--- lib/src/presenter/pages/pokedex/view/pokedex.dart
+++ lib/src/presenter/pages/pokedex/view/pokedex.dart
@@ -10,35 +10,109 @@ class Pokedex extends StatelessWidget {
     return Scaffold(
       backgroundColor: AppColors.identity,
       body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 12),
-          child: Column(
-            children: [
-              // Ícone e título
-              Row(
-                children: [
-                  IconButton(
-                    onPressed: () {},
-                    icon: SvgPicture.asset(
-                      './assets/icons/Pokeball.svg',
-                      width: 35,
-                    ),
-                  ),
-                  const Padding(
-                    padding: EdgeInsets.symmetric(horizontal: 12),
-                    child: Text(
-                      'Pokédex',
-                      style: TextStyle(
-                        fontFamily: 'Poppins',
-                        fontSize: 32,
-                        fontWeight: FontWeight.w700,
-                        color: Colors.white,
+        child: GestureDetector(
+          onTap: () {
+            FocusScope.of(context).unfocus();
+          },
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 8),
+            child: Column(
+              children: [
+                // Parte superior
+                Column(
+                  children: [
+                    // Ícone e título
+                    Padding(
+                      padding: const EdgeInsets.fromLTRB(8, 8, 8, 0),
+                      child: Row(
+                        children: [
+                          IconButton(
+                            onPressed: () {},
+                            icon: SvgPicture.asset(
+                              './assets/icons/Pokeball.svg',
+                              width: 28,
+                            ),
+                          ),
+                          const Padding(
+                            padding: EdgeInsets.only(left: 10),
+                            child: Text(
+                              'Pokédex',
+                              style: TextStyle(
+                                fontFamily: 'Poppins',
+                                fontSize: 32,
+                                fontWeight: FontWeight.w700,
+                                color: Colors.white,
+                              ),
+                            ),
+                          )
+                        ],
                       ),
                     ),
-                  )
-                ],
-              )
-            ],
+
+                    // Barra de pesquisa e filtro
+                    Padding(
+                      padding: const EdgeInsets.fromLTRB(16, 8, 16, 24),
+                      child: Row(
+                        children: [
+                          // Barra de pesquisa
+                          Expanded(
+                            child: Padding(
+                              padding: const EdgeInsets.only(right: 16),
+                              child: TextFormField(
+                                decoration: InputDecoration(
+                                  filled: true,
+                                  fillColor: AppColors.grayscale['background'],
+                                  isDense: true,
+                                  contentPadding: const EdgeInsets.all(0),
+                                  prefixIcon: const Icon(
+                                    Icons.search,
+                                    color: AppColors.identity,
+                                    size: 32,
+                                  ),
+                                  hintText: 'Search',
+                                  border: OutlineInputBorder(
+                                    borderRadius: BorderRadius.circular(30),
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ),
+                          // Botão de filtro
+                          Container(
+                            decoration: BoxDecoration(
+                              color: AppColors.grayscale['background'],
+                              borderRadius: BorderRadius.circular(100),
+                            ),
+                            child: IconButton(
+                              onPressed: () {},
+                              icon: SvgPicture.asset(
+                                './assets/icons/sort.svg',
+                                width: 30,
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    )
+                  ],
+                ),
+
+                // Container dos cards
+                Expanded(
+                  child: Container(
+                    padding: const EdgeInsets.only(bottom: 10),
+                    decoration: BoxDecoration(
+                      color: AppColors.grayscale['background'],
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                  ),
+                ),
+
+                const SizedBox(
+                  height: 10,
+                ),
+              ],
+            ),
           ),
         ),
       ),

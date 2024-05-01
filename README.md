3d4ce21a58227af3b9001f2e46b0e157820bc52e
randradee <dev.renatopandrade@gmail.com>
2024-05-01 18:09:18 -0300

    setup: initial setup

---
+++ README.md
@@ -0,0 +1,6 @@
+# pokedex
+
+Flutter project based on PokéAPI
+
+- Based on [figma prototype](https://www.figma.com/file/dsDuAF13pAI8XSfodwdr2B/Pok%C3%A9dex-(Community)?type=design&node-id=0-1&mode=design&t=eKnEMSKVnnJTlZRF-0)
+- [PokéAPI](https://pokeapi.co/)
---
+++ lib/src/app_root.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class AppRoot extends StatelessWidget {
+  const AppRoot({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Pokedex',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const Placeholder(),
+    );
+  }
+}
